78b7bf232ea0450152bbd79bce991322e61d757b
Nigel Banks <nigel.g.banks@gmail.com>
2012-10-09 20:40:55 -0400

    Updated the plugin to remove a race condition where the object was created but its upload file had not yet become a datastream.

--- plugins/smithsonian_plugin.py
+++ plugins/smithsonian_plugin.py
@@ -10,38 +10,21 @@ class smithsonian_plugin(IslandoraListenerPlugin):
         # call the parent function (this just prints an init message to the logs
         # this is a good practice
         IslandoraListenerPlugin.initialize(self, config_parser)
-
         # setup a prettyprint object
         self.pp = pprint.PrettyPrinter(indent=4)
-
-        # get the demo section from the config
-        try:
-            data = config_parser.get('Custom', 'demo')
-
-            # log it
-            self.logger.info('Got this data from config file: "%s".' % data)
-
-        except ConfigParser.Error:
-            self.logger.exception('Parsing config file failed.')
-            return False
 
         return True
 
     def fedoraMessage(self, message, obj, client):
-        # Take a look at what content model we are dealing with
-        # do some fun stuff based on this
+        # On the Smithsonian site we upload the file datastreams after ingest, only responds to adding or modification purging shouldn't enter this block.
         pp = pprint.PrettyPrinter(indent=4)
-        if 'si:fieldbookCModel' in message['content_models'] and (message['dsid'] == 'OBJ' or message['method'] == 'ingest'):
-            if obj['OBJ'].mimeType == 'application/pdf':
-                DSC.create_swf(obj, 'OBJ', 'OBJ.swf', ['-T 9', '-f', '-t', '-s', 'storeallcharacters', '-G', '-s', 'subpixels=1.5'])
-                DSC.create_thumbnail(obj, 'OBJ', 'TN')
-        if 'si:imageCModel' in message['content_models'] and (message['dsid'] == 'IMAGE' or message['method'] == 'ingest'):
+        if 'si:fieldbookCModel' in message['content_models'] and message['dsid'] == 'OBJ' and obj['OBJ'].mimeType == 'application/pdf':
+            DSC.create_swf(obj, 'OBJ', 'OBJ.swf', ['-T 9', '-f', '-t', '-s', 'storeallcharacters', '-G', '-s', 'subpixels=1.5'])
+            DSC.create_thumbnail(obj, 'OBJ', 'TN')
+        if ('si:imageCModel' in message['content_models'] or 'si:generalImageCModel' in message['content_models']) and message['dsid'] == 'IMAGE':
             DSC.create_thumbnail(obj, 'IMAGE', 'TN')
             DSC.create_fits(obj, 'IMAGE')
-        if 'si:generalImageCModel' in message['content_models'] and (message['dsid'] == 'IMAGE' or message['method'] == 'ingest'):
-            DSC.create_thumbnail(obj, 'IMAGE', 'TN')
-            DSC.create_fits(obj, 'IMAGE')
-        if 'si:datasetCModel' in message['content_models'] and (message['dsid'] == 'OBJ' or message['method'] == 'ingest'):
+        if 'si:datasetCModel' in message['content_models'] and message['dsid'] == 'OBJ':
             if obj['OBJ'].mimeType == 'application/vnd.ms-excel':
                 DSC.create_csv(obj, 'OBJ', 'CSV')
             elif obj['OBJ'].mimeType == 'text/csv':
@@ -57,4 +40,3 @@ class smithsonian_plugin(IslandoraListenerPlugin):
         print message
         self.logger.info(message)
         print ""
-
